0530a10622170a40f9f550806707c5b2f6940f1a
shikai <shikai@semoor.cn>
2020-03-29 15:24:20 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,106 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <show-money :client="client" :money="money" @showKey="showKey"></show-money>
+    <key-board :visable.sync="show" :max-money="client.max_money" :money.sync="money" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import KeyBoard from './components/KeyBoard'
+import ShowMoney from './components/ShowMoney'
 export default {
-  name: 'app',
+  name: 'App',
   components: {
-    HelloWorld
+    ShowMoney,
+    KeyBoard
+  },
+  data() {
+    return {
+        client: {
+          client_id: '12345', // 店铺ID
+          client_name: '店铺标题名称', // 店铺名称
+          openid: 'hfwegfgdisf', // 用户openid
+          device_id: '123898', // 设备ID
+          device_name: '', // 设备名称
+          table_no: '02', // 桌号
+          pay_url: '', // 支付域名(环境域名 beta,dct)
+          openid_url: '', // ?
+          auth_code: '', // ?
+          total: '', // 金额
+          order_no: '', // 订单号
+          auto_pay: '', //自动支付
+          param: '', // 预支付信息
+          staff: '', // 服务员
+          // max_money: 0 //最大支付金额
+        },
+      show: true,
+      money: '',
+      msg: '询问服务员后输入'
+    }
+  },
+  methods: {
+    showKey() {
+      this.show = true
+    },
+    inputMoney(val) {
+      if (val === -1) return false;
+      switch (val) {
+        case '.':
+          this._handleDecimalPoint();
+          break;
+        default:
+          this._handleNumberKey(val);
+          break;
+      }
+    },
+    deleteMoney() {
+      let S = this.money;
+      //如果没有输入，直接返回
+      if (!S.length) return false;
+      //否则删除最后一个
+      this.money = S.substring(0, S.length - 1);
+    },
+    //处理小数点函数
+    _handleDecimalPoint() {
+      //如果包含小数点，直接返回
+      if (this.money.indexOf('.') > -1) return false;
+      //如果小数点是第一位，补0
+      if (!this.money.length)
+        this.money = '0.';
+      //如果不是，添加一个小数点
+      else
+        this.money = this.money + '.';
+    },
+    //处理数字
+    _handleNumberKey(num) {
+      let S = this.money;
+      //如果有小数点且小数点位数不小于2
+      if ( S.indexOf('.') > -1 && S.substring(S.indexOf('.') + 1).length < 2)
+        this.money = S + num;
+      //没有小数点
+      if (!(S.indexOf('.') > -1)) {
+        //如果第一位是0，只能输入小数点
+        if (num === 0 && S.length === 0)
+          this.money = '0.';
+        else {
+          if (S.length && Number(S.charAt(0)) === 0) return;
+          this.money = S + num;
+        }
+      }
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  body {
+    background: $bg-color;
+  }
+  *{
+    margin:0;
+    user-select:none;
+  }
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+// import router from './router'
+// import axios from 'axios'
+// import vueAxiox from 'vue-axios'
 
 Vue.config.productionTip = false
 
 new Vue({
+  // router,
   render: h => h(App),
+  mounted: () => document.dispatchEvent(new Event("x-app-rendered")),
 }).$mount('#app')
